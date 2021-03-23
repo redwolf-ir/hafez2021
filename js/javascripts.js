cc94a040ea7852a4bdcd5780a25d7fed4718b647
Red Wolf ir <redwolf_ir@outlook.com>
2021-03-23 21:08:36 +0430

    scrolling snapp just added

--- js/javascripts.js
+++ js/javascripts.js
@@ -90,15 +90,59 @@ tl.to(
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".welcome-part h2", {
+// gsap.fromTo(
+//   ".welcome-part h2",
+//   { opacity: 0, x: 0 },
+//   {
+//     scrollTrigger: {
+//       // markers: true,
+//       // scrub: true,
+//       trigger: ".welcome-part h2",
+//       start: "top bottom", // avali object, dovomi box
+//       toggleActions: "restart none none pause",
+//     },
+//     y: "-50px",
+//     opacity: 1,
+//     duration: 1.5,
+//   }
+// );
+
+gsap.to(".welcome-part", {
   scrollTrigger: {
-    markers: true,
-    // scrub: true,
-    trigger: ".welcome-part h2",
-    start: "bottom bottom", // avali object, dovomi box
+    scrub: true,
+    // markers: true,
+    trigger: ".welcome-part",
+    start: "top top",
     toggleActions: "restart none none none",
   },
-  y: -30,
-  opacity: 1,
-  duration: 1.5,
+  borderTopLeftRadius: "0",
+  borderTopRightRadius: "0",
+  duration: 0.1,
 });
+gsap.to(".introduction-part", {
+  scrollTrigger: {
+    scrub: true,
+    // markers: true,
+    trigger: ".introduction-part",
+    start: "-200px top",
+    toggleActions: "restart none none none",
+  },
+  borderTopLeftRadius: "0",
+  borderTopRightRadius: "0",
+  duration: 0.1,
+});
+
+// var defaultOptions = {
+//   container: document.body,
+//   panelSelector: "> aside",
+//   directionThreshold: 50,
+//   delay: 0,
+//   duration: 300,
+//   easing: function (t) {
+//     return t;
+//   },
+// };
+
+// new PanelSnap(options);
+var scrollSpeed = 250;
+var scrollWait = 30250;
